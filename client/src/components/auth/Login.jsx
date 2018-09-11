dece917743e05b2ac8c0ecd1dac972e4e1484d94
SMuyesser <smuyesser@gmail.com>
2018-09-11 16:37:53 -0700

    login and token in local storage finalized

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { loginUser } from "../../actions/authActions";
 
 class Login extends Component {
 	constructor() {
 		super();
 		this.state = {
 			email: "",
-			password: ""
+			password: "",
+			errors: {}
 		};
 	}
 
@@ -19,18 +24,31 @@ class Login extends Component {
 		});
 	};
 
+	// We get errors from redux state and gets put into props with mapstatetoprops
+	// once recieve new properties and errors is included, then it gets set to component state
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.auth.isAuthenticated) {
+			this.props.history.push("/dashboard");
+		}
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
+	}
+
 	onSubmit = e => {
 		e.preventDefault();
 
-		const loginInfo = {
+		const userData = {
 			email: this.state.email,
 			password: this.state.password
 		};
 
-		console.log(loginInfo);
+		this.props.loginUser(userData);
 	};
 
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div className="login">
 				<div className="container">
@@ -44,22 +62,42 @@ class Login extends Component {
 								<div className="form-group">
 									<input
 										type="email"
-										className="form-control form-control-lg"
+										className={classnames(
+											"form-control form-control-lg",
+											{
+												"is-invalid": errors.email
+											}
+										)}
 										placeholder="Email Address"
 										name="email"
 										value={this.state.email}
 										onChange={this.onChange}
 									/>
+									{errors.email && (
+										<div className="invalid-feedback">
+											{errors.email}
+										</div>
+									)}
 								</div>
 								<div className="form-group">
 									<input
 										type="password"
-										className="form-control form-control-lg"
+										className={classnames(
+											"form-control form-control-lg",
+											{
+												"is-invalid": errors.password
+											}
+										)}
 										placeholder="Password"
 										name="password"
 										value={this.state.password}
 										onChange={this.onChange}
 									/>
+									{errors.password && (
+										<div className="invalid-feedback">
+											{errors.password}
+										</div>
+									)}
 								</div>
 								<input
 									type="submit"
@@ -74,4 +112,18 @@ class Login extends Component {
 	}
 }
 
-export default Login;
+Login.propTypes = {
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+	errors: state.errors
+});
+
+export default connect(
+	mapStateToProps,
+	{ loginUser }
+)(Login);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,7 @@
+import isEmpty from "../validation/is-empty.js";
+
+import { SET_CURRENT_USER } from "../actions/types";
+
 const initialState = {
 	isAuthenticated: false,
 	user: {}
@@ -5,6 +9,13 @@ const initialState = {
 
 export default function(state = initialState, action) {
 	switch (action.type) {
+		case SET_CURRENT_USER:
+			return {
+				...state,
+				// isAuthenticated depends on the payload not being empty
+				isAuthenticated: !isEmpty(action.payload),
+				user: action.payload
+			};
 		default:
 			return state;
 	}
