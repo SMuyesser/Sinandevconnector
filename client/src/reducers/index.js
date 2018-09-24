8a81b95b9acdafee6412574841cef182008a7978
SMuyesser <smuyesser@gmail.com>
2018-09-24 15:48:46 -0700

    reducers index file updated for profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import profileReducer from "./profileReducer";
 
 export default combineReducers({
 	// Object with all of our reducers
 	// Accessed later with this.props.auth
 	auth: authReducer,
-	errors: errorReducer
+	errors: errorReducer,
+	profile: profileReducer
 });
