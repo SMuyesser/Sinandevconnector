aec5bf75373f44a648c62ef3aa3e133dd7125b33
SMuyesser <smuyesser@gmail.com>
2018-09-24 16:02:09 -0700

    updated app for loading and unloading profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from './actions/profileActions.js';
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -30,7 +31,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    //TODO: Clear current profile
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
   }
