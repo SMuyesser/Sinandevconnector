98fe06eb644892eee164a5a55485d26b1df1f471
SMuyesser <smuyesser@gmail.com>
2018-12-05 12:22:27 -0800

    experience component now displaying and deleting experience

--- client/src/components/dashboard/Experience.jsx
+++ client/src/components/dashboard/Experience.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
+import { deleteExperience } from '../../actions/profileActions';
 
 class Experience extends Component {
+	onDeleteclick(id) {
+		this.props.deleteExperience(id);
+	}
+
 	render() {
 		const experience = this.props.experience.map(exp => (
 			<tr key={exp._id}>
 				<td>{exp.company}</td>
 				<td>{exp.title}</td>
-				<td>{exp.from} - {exp.to}</td>
-				<td><button className="btn btn-danger">Delete</button></td>
+				<td>
+					<Moment format="YYYY/MM/DD">{exp.from}</Moment> -   
+					{exp.to === null ? (' Current') : <Moment format="YYYY/MM/DD">{exp.to}</Moment>}
+				</td>
+				<td><button onClick={this.onDeleteclick.bind(this, exp._id)} className="btn btn-danger">Delete</button></td>
 			</tr>
 		))
 
@@ -33,4 +41,8 @@ class Experience extends Component {
 	}
 }
 
-export default connect(null)(withRouter(Experience));
+Experience.propTypes = {
+	deleteExperience: PropTypes.func.isRequired
+}
+
+export default connect(null, { deleteExperience })(Experience);
