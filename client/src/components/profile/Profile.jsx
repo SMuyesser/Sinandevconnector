8f04f1a6f3cc6ab0c2cbff0b9117a53e996008da
SMuyesser <smuyesser@gmail.com>
2018-12-12 14:55:41 -0800

    profile display component finalized

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -18,12 +18,38 @@ class Profile extends Component {
 	}
 
 	render() {
+		const { profile, loading } = this.props.profile;
+		let profileContent;
+
+		if(profile === null || loading) {
+			profileContent = <Spinner />
+		} else {
+			profileContent = (
+				<div>
+					<div className="row">
+						<div className="col-md-6">
+							<Link to ="/profiles" className="btn btn-light mb-3 float float-left">
+								Back To Profiles
+							</Link>
+						</div>
+						<div className="col-md-6" />
+					</div>
+					<ProfileHeader profile={profile}/>
+					<ProfileAbout />
+					<ProfileCreds />
+					<ProfileGithub />
+				</div>
+			)
+		}
 		return (
-			<div>
-				<ProfileHeader />
-				<ProfileAbout />
-				<ProfileCreds />
-				<ProfileGithub />
+			<div className="profile">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							{profileContent}
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
@@ -34,8 +60,8 @@ Profile.propTypes = {
 	getProfileByHandle: PropTypes.func.isRequired
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
 	profile: state.profile
-}
+})
 
 export default connect(mapStateToProps, { getProfileByHandle })(Profile);
