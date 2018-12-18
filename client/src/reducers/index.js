a8d2c14e503a414c72d641ad7037e95058947682
SMuyesser <smuyesser@gmail.com>
2018-12-17 22:22:04 -0800

    post added to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
+
+
 
 export default combineReducers({
 	// Object with all of our reducers
 	// Accessed later with this.props.auth
 	auth: authReducer,
 	errors: errorReducer,
-	profile: profileReducer
+	profile: profileReducer,
+	post: postReducer
 });
