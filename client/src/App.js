96b1da2e7afeef446a941750c1cee3c5288eb360
SMuyesser <smuyesser@gmail.com>
2018-12-18 18:50:28 -0800

    app.js updated with post route

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
 
 import "./App.css";
 
@@ -78,6 +79,9 @@ class App extends Component {
               </Switch>              
               <Switch>
                 <Route exact path="/not-found" component={NotFound} />
+              </Switch>              
+              <Switch>
+                <Route exact path="/feed" component={Posts} />
               </Switch>
             </div>
             <Footer />
