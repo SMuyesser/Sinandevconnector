9b39c2125c5518a7ed933f88969c8d1699529a94
SMuyesser <smuyesser@gmail.com>
2019-01-09 09:47:38 -0800

    post updated to include comment form

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -15,12 +16,13 @@ class Post extends Component {
 		const { post, loading } = this.props.post;
 		let postContent;
 
-		if(post === null || loading || Object.keys(post).legnth === 0) {
+		if(post === null || loading || Object.keys(post).length === 0) {
 			postContent = <Spinner />
 		} else {
 			postContent = (
 				<div>
 					<PostItem post={post} showActions={false} />
+					<CommentForm postId={post._id} />
 				</div>
 			);
 		}
